9a7fb683956ba0e44bbb9c61b03a9d7730c6684e
AlanViast <AlanViast@gmail.com>
2024-06-05 12:04:11 +0800

    feat: BatchWorkExecutor concurrency mode

--- ethereumetl/executors/batch_work_executor.py
+++ ethereumetl/executors/batch_work_executor.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 import logging
+import os
 import time
 
 from requests.exceptions import Timeout as RequestsTimeout, HTTPError, TooManyRedirects
@@ -28,6 +29,7 @@ from web3._utils.threads import Timeout as Web3Timeout
 
 from ethereumetl.executors.bounded_executor import BoundedExecutor
 from ethereumetl.executors.fail_safe_executor import FailSafeExecutor
+from ethereumetl.executors.mode import BatchMode
 from ethereumetl.misc.retriable_value_error import RetriableValueError
 from ethereumetl.progress_logger import ProgressLogger
 from ethereumetl.utils import dynamic_batch_iterator
@@ -37,6 +39,7 @@ RETRY_EXCEPTIONS = (ConnectionError, HTTPError, RequestsTimeout, TooManyRedirect
 
 BATCH_CHANGE_COOLDOWN_PERIOD_SECONDS = 2 * 60
 
+mode = os.getenv("BATCH_MODE", "Batch")
 
 # Executes the given work in batches, reducing the batch size exponentially in case of errors.
 class BatchWorkExecutor:
@@ -55,6 +58,12 @@ class BatchWorkExecutor:
 
     def execute(self, work_iterable, work_handler, total_items=None):
         self.progress_logger.start(total_items=total_items)
+
+        if BatchMode[mode.capitalize()] == BatchMode.Concurrency:
+            for item in work_iterable:
+                self.executor.submit(self._fail_safe_execute, work_handler, [item])
+            return
+
         for batch in dynamic_batch_iterator(work_iterable, lambda: self.batch_size):
             self.executor.submit(self._fail_safe_execute, work_handler, batch)
 
